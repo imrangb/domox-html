8fcca3a0632530121a80a7f7262e4e95276344d9
Mdi. Imran Hossain <imranrazahmed@gmail.com>
2021-02-17 23:29:17 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+  
+  //$(document).ready(function(){});
+  
+  (function ($) {
+    "use strict";
+
+    
+
+    $('.spnsor-items').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidestoScroll:1,
+     
+    });
+    
+
+
+
+
+
+})(jQuery);	    
+
+
